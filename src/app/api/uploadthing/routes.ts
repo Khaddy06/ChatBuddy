097353ca0473e6978858853e2cb23f94fbd41988
khaddy06 <harameedey06@gmail.com>
2025-07-23 15:44:00 +0100

    removed few files

--- src/app/api/uploadthing/routes.ts
+++
@@ -1,8 +0,0 @@
-// app/api/uploadthing/route.ts
-import { createRouteHandler } from "uploadthing/next";
-import { ourFileRouter } from "./core";
-
-// Export API route handlers
-export const { GET, POST } = createRouteHandler({
-  router: ourFileRouter,
-});
--- src/app/utils/uploadthing.ts
+++
@@ -1,7 +0,0 @@
-// // utils/uploadthing.ts
-// import { UploadButton, UploadDropzone } from "@uploadthing/react"; // ✅ Import directly
-// import { generateReactHelpers } from "@uploadthing/react";
-// import type { OurFileRouter } from "@/lib/uploadthing";
-
-// export const { useUploadThing } = generateReactHelpers<OurFileRouter>(); // ✅ only get this from helpers
-
